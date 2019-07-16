3767242c7acc7fa227ec58c105b4af8b3ded4546
blaze <kontaktAK@Yandex.ru>
2019-07-16 11:39:51 +0300

    ZipSearch component

---
+++ src/components/ZipSearch.vue
@@ -0,0 +1,49 @@
+<template>
+	<ion-grid>
+		<form @submit.prevent="onSubmit">
+			<ion-col>
+				<ion-item>
+					<ion-label>Zipcode</ion-label>
+					<ion-input :value="zip" @input="zip = $event.target.value" placeholder="Введите почтовый индекс"
+					           name="zip"/>
+				</ion-item>
+			</ion-col>
+			<ion-col>
+				<ion-button type="submit" color="primary" expand="block"> Find</ion-button>
+			</ion-col>
+		</form>
+	</ion-grid>
+</template>
+
+<script>
+  /* eslint-disable */
+  export default {
+    name: 'ZipSearch',
+    data () {
+      return {
+        zip: ''
+      }
+    },
+    methods: {
+      onSubmit () {
+        // валидируем
+        const isCodeValid = /^\d{6}$/.test(this.zip)
+        // если зипкод некорректный, выводим сообщение
+        if (!isCodeValid) return this.showAlert()
+
+        this.$emit('get-zip', this.zip)
+        this.zip = ''
+
+      },
+      showAlert () {
+        return this.$ionic.alertController
+                   .create({
+                     header: 'Введите почтовый индекс',
+                     message: 'Пожалуйста, введите корректный почтовый индекс',
+                     buttons: ['OK']
+                   })
+                   .then(a => a.present())
+      }
+    }
+  }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="ion-page">
+		<ion-header>
+			<ion-toolbar>
+				<ion-title> VueIonic</ion-title>
+			</ion-toolbar>
+		</ion-header>
+		<ion-content class="ion-padding">
+			<zip-search @get-zip="getZipInfo"/>
+		</ion-content>
+
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  import ZipSearch from '../components/ZipSearch'
+
+  export default {
+    name: 'home',
+    components: {
+      ZipSearch
+    },
+    data () {
+      return {
+        info: null
+      }
+    },
+    methods: {
+      async getZipInfo (zip) {
+
+        const response = await fetch(`http://api.zippopotam.us/RU/${zip}`)
+        if (response.status === 404) return this.showAlert()
+
+        this.info = await response.json()
+
+
+      },
+      showAlert () {
+        return this.$ionic.alertController
+                   .create({
+                     header: 'Почтовый индекс не найден',
+                     message: 'Пожалуйста, введите корректный почтовый индекс',
+                     buttons: ['OK']
+                   })
+                   .then(a => a.present())
+      }
+    }
   }
-}
 </script>
